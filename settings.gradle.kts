dc21c4c5a74fded4bb39eebc9f868e6c887c66d8
Yuri Moens <yuri.moens@gmail.com>
2022-08-22 23:54:20 +0200

    project: remove testing include

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -49,10 +49,6 @@ include("utils")
 include("zmi")
 include("zulrah")
 
-if (File("./test").exists()) {
-    include("test")
-}
-
 for (project in rootProject.children) {
     project.apply {
         projectDir = file(name)
